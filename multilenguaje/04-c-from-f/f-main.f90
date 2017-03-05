9206633fd7496e6c663fcff264e7e6a3b5d686fc
franco <franco@WTPC>
2017-03-05 16:57:06 -0300

    subo los problemas para diego.sigue sin andar el 04-c-from-f

--- multilenguaje/04-c-from-f/f-main.f90
+++ multilenguaje/04-c-from-f/f-main.f90
@@ -1,13 +1,14 @@
 program testsum
+! En este codigo queremos combinar codigo fortran llamando alguna funcion de C
   implicit none
 
   integer, parameter :: n=200
-  integer :: data(n), asum, i
-
+  integer :: data(n), asum, i , a
+  integer :: sum_abs 
   do i=1,200
     data(i) = i-100
   end do
-
-  call sum_abs(data,n,asum)
-  print*, 'sum=',asum
+  asum = 0  
+  a = sum_abs(data,n,asum)
+  print*, a,sum_abs(data,n,asum)
 end program testsum 
